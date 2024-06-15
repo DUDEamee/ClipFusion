5071105154de4babcd34d367785ba4d1b5571621
DUDEamee <ameenameen8701@gmail.com>
2024-06-15 19:30:22 +0530

    added : Banner,css,js,img also added, and gradient

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import NavBar from './Componenets/Navbar/NavBar';
 import "./App.css"
+import Banner from './Componenets/Banner/Banner';
+import RowPost from './Componenets/RowPost/RowPost';
 function App() {
   return (
     <div className="App">
       <header>
         <NavBar/>
       </header>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
--- src/Componenets/Banner/Banner.js
+++ src/Componenets/Banner/Banner.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import "./Banner.css"
 const Banner = () => {
   return (
-    <div>
-      
+    <div className='banner'>
+      <div className='content'>
+        <h1 className='title'>Movie Name</h1>
+        <div className='banner_buttons'>
+          <button className='button'>Play</button>
+          <button className='button'>My List</button>
+        </div>
+        <h1 className='description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h1>
+      </div>
+      <div className="fadeBottom">
+
+      </div>
     </div>
   )
 }
