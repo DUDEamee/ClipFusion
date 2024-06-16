842a23af882e66dc32fe56a54e946a74b603be52
DUDEamee <ameenameen8701@gmail.com>
2024-06-16 19:16:15 +0530

    added : useState and useEffect

--- src/Componenets/Banner/Banner.js
+++ src/Componenets/Banner/Banner.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_KEY} from '../../Constants/constants'
+import axios from '../../axios'
 import "./Banner.css"
 const Banner = () => {
+  const [movie, setMovie] = useState([])
+  useEffect(()=>{
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((responce)=>{
+      console.log(responce.data.results[0])
+      setMovie(responce.data.results[0])
+    })
+  },[])
   return (
     <div className='banner'>
       <div className='content'>
---
+++ src/Constants/constants.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '4be4ab59f8118b0cf8dd75efe8381b78';
